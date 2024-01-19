474df6a54a6251ee32ba3f98abd690e7d43b4071
GitKunalHub <kunalgolani01@gmail.com>
2024-01-19 17:31:08 +0530

    Implemented Search Input

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -6,12 +6,28 @@ type SearchInputProps = {};
 
 const SearchInput: React.FC<SearchInputProps> = () => {
   return (
-    <Flex>
+    <Flex flexGrow={1} mr={2} align="center">
       <InputGroup>
         <InputLeftElement pointerEvents="none">
-          <SearchIcon color="gray.300" />
+          <SearchIcon color="gray.400" mb={1} />
         </InputLeftElement>
-        <Input type="tel" placeholder="Search Gravity" />
+        <Input
+          placeholder="Search Gravity"
+          fontSize="10pt"
+          _placeholder={{ color: "gray.500" }}
+          _hover={{
+            bg: "white",
+            border: "1px solid",
+            borderColor: "green.500",
+          }}
+          _focus={{
+            outline: "none",
+            border: "1px solid",
+            borderColor: "green.500",
+          }}
+          height="34px"
+          bg="gray.50"
+        />
       </InputGroup>
     </Flex>
   );
